21bb4d70921d6fef2b9462779b7937543e060419
brayantorres <brayantorres9431@gmail.com>
2018-10-11 12:25:43 -0500

    Agrego metodos faltantes

--- src/main/java/stepDefinitions/registroStep.java
+++ src/main/java/stepDefinitions/registroStep.java
@@ -1,10 +1,14 @@
 package stepDefinitions;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
 
 public class registroStep {
 	
@@ -19,5 +23,27 @@ public class registroStep {
 	public void tearDown() {
 		driver.quit();
 	}
+	
+	@Given("El usuario esta en la pagina registro")
+    public void usuarioEstaEnRegistro () {
+    	driver.get("http://sdettraining.com/trguitransactions/NewAccount.aspx");
+    }
+	
+	@When("el usuario ingresa sus datos ")
+    public void usuarioIngresaSusDastos () {
+    	driver.findElement(By.id("MainContent_txtFirstName")).sendKeys("Brayan David Torres Suarez");
+    	driver.findElement(By.id("MainContent_txtEmail")).sendKeys("brayantorres@unac.edu.co");
+    	driver.findElement(By.id("MainContent_txtHomePhone")).sendKeys("3012363749");
+    	driver.findElement(By.id("MainContent_Male")).click();
+    	driver.findElement(By.id("MainContent_txtPassword")).sendKeys("asd$123");
+    	driver.findElement(By.id("MainContent_txtVerifyPassword")).sendKeys("asd$123");
+    	
+    	Select dropdown = new Select(driver.findElement(By.id("MainContent_menuCountry")));
+    	dropdown.selectByValue("Ecuador");
+    	
+    	driver.findElement(By.id("MainContent_checkMonthlyEmail")).click();
+    	
+    	driver.findElement(By.id("MainContent_btnSubmit")).click();
+    }
 
 }
