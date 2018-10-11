48c262d78aa5a6a3243302c35d302fe7780da939
brayantorres <brayantorres9431@gmail.com>
2018-10-11 10:49:23 -0500

    Subo clase registro usuario

--- src/main/java/stepDefinitions/registroStep.java
+++ src/main/java/stepDefinitions/registroStep.java
@@ -1,5 +1,23 @@
 package stepDefinitions;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
 public class registroStep {
+	
+	WebDriver driver; 
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+    	driver = new ChromeDriver();
+	}
+	
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
 
 }
